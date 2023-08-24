f96ac1ad4e14b81eb4e391bb8d19ba6d4b5e3e1f
MUScphillips <141957841+MUScphillips@users.noreply.github.com>
2023-08-24 08:25:31 -0500

    setup GradeAveraging.java

---
+++ src/GradeAveraging.java
@@ -0,0 +1,20 @@
+public class GradeAveraging
+{
+    public static void main(String[] args)
+    {
+         // 1. Declare 3 int variables called grade1, grade2, grade3
+        // and initialize them to 3 values
+
+        // 2. Declare an int variable called sum for the sum of the grades
+
+        // 3. Declare a variable called average for the average of the grades
+
+        // 4. Write a formula to calculate the sum of the 3 grades (add them
+        // up).
+
+        // 5. Write a formula to calculate the average of the 3 grades from
+        // the sum using division and type casting.
+
+        // 6. Print out the average
+    }
+}
